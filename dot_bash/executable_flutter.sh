ae12bab02ee9df9738f1a4a0e3f644e820780572
Vinoth Ramiah <vin-git.1u18t@simplelogin.com>
2025-07-16 20:31:49 +0100

    add .bash/flutter.sh

---
+++ dot_bash/executable_flutter.sh
@@ -0,0 +1,14 @@
+# flutter
+export FLUTTER_ROOT=/opt/flutter        # SDK install directory
+export PATH=$FLUTTER_ROOT/bin:$PATH
+
+# dart
+export PUB_CACHE=/usr/local/flutter/pub-cache
+
+# fvm
+export FVM_CACHE_PATH=/usr/local/flutter/fvm
+
+## [Completion]
+## Completion scripts setup. Remove the following line to uninstall
+[ -f /Users/vin/.dart-cli-completion/bash-config.bash ] && . /Users/vin/.dart-cli-completion/bash-config.bash || true
+## [/Completion]
