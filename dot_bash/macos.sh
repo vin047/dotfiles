c6e714d9c4a7e5198682ccf62b8d473106a91740
Vinoth Ramiah <vin-git.1u18t@simplelogin.com>
2025-08-13 19:00:08 +0100

    add .bash/macos.sh

---
+++ dot_bash/macos.sh
@@ -0,0 +1,6 @@
+# macports
+export PATH="/opt/local/bin:/opt/local/sbin:$PATH"
+export MANPATH="/opt/local/share/man:$MANPATH"
+
+# GNU binaries via macports
+export PATH="/opt/local/libexec/gnubin:$PATH"
